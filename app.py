58b505c663a6042e293b10c0ae5c55d44e105fc0
Jan Nunez <theprobingteep@protonmail.com>
2023-12-08 14:24:36 -0500

    Remove main

--- app.py
+++ app.py
@@ -47,7 +47,3 @@ async def predict(file: UploadFile):
         return {"prediction": predicted_class, "confidence": confidence}
     except Exception as e:
         return {"error": str(e)}
-    
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
